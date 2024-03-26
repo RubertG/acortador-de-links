db4918a4bc2194625aef1a61e19443e9488c40c0
Rubert_G <rubertgonzalez5@gmail.com>
2024-03-26 14:55:58 -0500

    🛠️ metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import './globals.css'
 import { poppins } from '@/fonts/fonts'
 
 export const metadata: Metadata = {
-  title: 'Links cortos - Rubert Gonzalez',
-  description: 'Acorta tus links como desees. Desarrollado por Rubert Gonzalez'
+  title: 'Acortador de enlaces - Rubert Gonzalez',
+  description: 'Abrevia y personaliza tu URL de manera totalmente gratuita. Podrás realizar un seguimiento de todos los clics que se hagan en tu enlace y ver la fecha en que se llevaron a cabo. Además, tienes la opción de utilizar enlaces abreviados de otros usuarios para mayor comodidad. Desarrollado por Rubert Gonzalez'
 }
 
 export default function RootLayout ({
