f43e353ac73260bc1145d8daa8dbc3cad57e9b78
Rubert_G <rubertgonzalez5@gmail.com>
2024-04-04 16:06:21 -0500

    🔧 numero de items por página de la tabla

--- src/types/const.ts
+++ src/types/const.ts
@@ -6,4 +6,4 @@ export const NAME_CHECKBOX = {
 export const DAYS_GRAPH_VISIT_LINKS = 15
 export const TIME_GRAPH_VISIT_LINKS = DAYS_GRAPH_VISIT_LINKS * 24 * 60 * 60 * 1000
 
-export const ITEMS_FOR_PAGE_IN_TABLE = 10
+export const ITEMS_FOR_PAGE_IN_TABLE = 15
